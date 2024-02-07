1671903fa81b45a6934db13ea01035a452907222
Styn van de Haterd <chaosnova2011@hotmail.com>
2024-02-07 12:51:18 +0100

    Added --output folder option.

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,9 @@ struct Args {
     folder: Option<String>,
 
     #[arg(long)]
+    output: Option<String>,
+
+    #[arg(long)]
     from: Option<String>,
 
     #[arg(long)]
@@ -29,20 +32,21 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
+    let output = args.output;
 
     if let Some(file) = args.file{
-        handle_image_file(&file, &args.to, 1, 1);
+        handle_image_file(&file, &args.to, 1, 1, &output);
     }
 
     if let Some(folder) = args.folder{
         if args.all {
-            handle_folder_all(&folder, &args.to);
+            handle_folder_all(&folder, &args.to, &output);
             return;
         }
 
         match args.from {
             Some(extension) => {
-                handle_folder(&folder, &extension, &args.to);
+                handle_folder(&folder, &extension, &args.to, &output);
             }
             None => {
                 eprintln!("ImageFlipper: [Expecting --from argument specifying extensions to convert from.]");
@@ -51,7 +55,7 @@ fn main() {
     }
 }
 
-fn handle_folder_all(folder: &str, to: &str){
+fn handle_folder_all(folder: &str, to: &str, output_folder: &Option<String>){
     let entries = fs::read_dir(folder)
         .unwrap_or_else(|err| {
             eprintln!("ImageFlipper: [Failed to read directory: {}]", err);
@@ -65,10 +69,10 @@ fn handle_folder_all(folder: &str, to: &str){
         )
         .collect::<Vec<_>>();
 
-    handle_image_files(&entries, to);
+    handle_image_files(&entries, to, output_folder);
 }
 
-fn handle_folder(folder: &str, from: &str, to: &str) {
+fn handle_folder(folder: &str, from: &str, to: &str, output_folder: &Option<String>) {
     let entries = fs::read_dir(folder)
         .unwrap_or_else(|err| {
             eprintln!("ImageFlipper: [Failed to read directory: {}]", err);
@@ -77,13 +81,13 @@ fn handle_folder(folder: &str, from: &str, to: &str) {
         .map(|res| res.unwrap())
         .filter(|entry|
             entry.path().is_file() &&
-                entry.path().extension().and_then(|ext| ext.to_str()) == Some(&*from))
+            entry.path().extension().and_then(|ext| ext.to_str()) == Some(&*from))
         .collect::<Vec<_>>();
 
-    handle_image_files(&entries, to);
+    handle_image_files(&entries, to, output_folder);
 }
 
-fn handle_image_files(entries: &Vec<DirEntry>, to: &str) {
+fn handle_image_files(entries: &Vec<DirEntry>, to: &str, output_folder: &Option<String>) {
     let counter = AtomicU16::new(0);
     let item_total_count = entries.len();
     entries.par_iter().for_each(|entry|{
@@ -91,7 +95,7 @@ fn handle_image_files(entries: &Vec<DirEntry>, to: &str) {
         let item_current_count = counter.load(SeqCst);
         let path = entry.path();
         let path_string = path.to_str().unwrap();
-        handle_image_file(path_string, to, item_current_count, item_total_count as u16);
+        handle_image_file(path_string, to, item_current_count, item_total_count as u16, output_folder);
     });
 }
 
@@ -112,10 +116,10 @@ fn is_image_format(extension: Option<&str>) -> bool {
     }
 }
 
-fn handle_image_file(file: &str, to: &str, item_current_count: u16, item_total_count: u16) {
+fn handle_image_file(file: &str, to: &str, item_current_count: u16, item_total_count: u16, output_folder: &Option<String>) {
     match load_image(&file){
         Ok(image) => {
-            convert_image(&file, &image, &to, item_current_count, item_total_count);
+            convert_image(&file, &image, &to, item_current_count, item_total_count, output_folder);
         }
         Err(e) => {
             eprintln!("ImageFlipper: [Failed to load image: {}]", e);
@@ -123,17 +127,27 @@ fn handle_image_file(file: &str, to: &str, item_current_count: u16, item_total_c
     }
 }
 
-fn convert_image(file: &str, image: &DynamicImage, to: &str, item_current_count: u16, item_total_count: u16) {
+fn convert_image(file: &str, image: &DynamicImage, to: &str, item_current_count: u16, item_total_count: u16, output_folder: &Option<String>) {
     let path = Path::new(file);
-    let mut path_buf = PathBuf::from(path);
-    path_buf.set_extension(to);
-
     let image_format = get_image_format(to);
+
     match image_format {
         Some(format) => {
-            println!("{}", format!("ImageFlipper: [Converting image (#{} of {}) {:?} to .{}]", item_current_count, item_total_count, path_buf.file_stem().unwrap(), to));
             let rgb_image = image.to_rgb8();
-            rgb_image.save_with_format(path_buf, format).expect(&*format!("ImageFlipper: [Error when converting file: {file}]"));
+            match output_folder {
+                Some(output_path) => {
+                    let mut new_path_buf = change_directory(path, Path::new(output_path));
+                    new_path_buf.set_extension(to);
+                    println!("{}", format!("ImageFlipper: [Converting image (#{} of {}) {:?} to .{}]", item_current_count, item_total_count, new_path_buf.file_stem().unwrap(), to));
+                    rgb_image.save_with_format(new_path_buf, format).expect(&*format!("ImageFlipper: [Error when converting file: {file}]"));
+                }
+                None => {
+                    let mut path_buf = PathBuf::from(path);
+                    path_buf.set_extension(to);
+                    println!("{}", format!("ImageFlipper: [Converting image (#{} of {}) {:?} to .{}]", item_current_count, item_total_count, path_buf.file_stem().unwrap(), to));
+                    rgb_image.save_with_format(path_buf, format).expect(&*format!("ImageFlipper: [Error when converting file: {file}]"));
+                }
+            }
         }
         _ => {
             eprintln!("ImageFlipper: [Unable to save image.]");
@@ -159,4 +173,31 @@ fn get_image_format(format: &str) -> Option<ImageFormat>{
 
 fn load_image(file: &str) -> Result<DynamicImage, ImageError> {
     ImageReader::open(file)?.decode()
+}
+
+fn change_directory(path: &Path, new_dir: &Path) -> PathBuf {
+    let file_name = path.file_name().expect("ImageFlipper: [Original path does not have a file name]");
+
+    if new_dir.is_absolute() {
+        let mut new_path = PathBuf::from(new_dir);
+        new_path.push(file_name);
+        create_directory_if_not_exists(&new_path);
+        new_path
+    } else {
+        let mut new_path = path
+            .parent()
+            .expect("ImageFlipper: [Original path does not have a parent directory]")
+            .to_path_buf();
+
+        new_path.push(new_dir);
+        create_directory_if_not_exists(&new_path);
+        new_path.push(file_name);
+        new_path
+    }
+}
+
+fn create_directory_if_not_exists(path: &PathBuf) {
+    if !Path::new(&path).exists() {
+        fs::create_dir_all(path).expect("ImageFlipper: [Unable to create missing output directory]");
+    }
 }
